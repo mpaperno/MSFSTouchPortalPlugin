b6e4029cc6551a190e5f806531184f65c8571aec
Max Paperno <max@wdg.us>
2022-07-13 02:38:46 -0400

    Tag source files with copyright & license headers. No code changes.

--- MSFSTouchPortalPlugin/Configuration/HubHop/HubHopType.cs
+++ MSFSTouchPortalPlugin/Configuration/HubHop/HubHopType.cs
@@ -1,4 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿/*
+This file is part of the MSFS Touch Portal Plugin project.
+https://github.com/mpaperno/MSFSTouchPortalPlugin
+
+COPYRIGHT: (c) Maxim Paperno; All Rights Reserved.
+
+This file may be used under the terms of the GNU General Public License (GPL)
+as published by the Free Software Foundation, either version 3 of the Licenses,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU GPL is included with this project
+and is also available at <http://www.gnu.org/licenses/>.
+*/
+
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
 
 namespace MSFSTouchPortalPlugin.Configuration
--- MSFSTouchPortalPlugin/Types/TouchPortalEvent.cs
+++ MSFSTouchPortalPlugin/Types/TouchPortalEvent.cs
@@ -1,4 +1,24 @@
-﻿using MSFSTouchPortalPlugin.Enums;
+﻿/*
+This file is part of the MSFS Touch Portal Plugin project.
+https://github.com/mpaperno/MSFSTouchPortalPlugin
+
+COPYRIGHT:
+(c) Maxim Paperno; All Rights Reserved.
+
+This file may be used under the terms of the GNU General Public License (GPL)
+as published by the Free Software Foundation, either version 3 of the Licenses,
+or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU GPL is included with this project
+and is also available at <http://www.gnu.org/licenses/>.
+*/
+
+using MSFSTouchPortalPlugin.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
