17a14d2fa07b94f0dd2d56cd475b36ea4dd158d3
Max Paperno <max@wdg.us>
2022-07-25 00:48:31 -0400

    [project] Clean up project root folder a bit; Embed debug info in release/publish builds but strip file paths.

--- publish.ps1
+++ publish.ps1
@@ -6,7 +6,7 @@ Param(
   [string]$Configuration = "Publish",
   [string]$Platform = "x64",
   [String]$VersionSuffix = "",
-  [switch]$Clean = $false,
+  [switch]$NoClean = $false,
   [switch]$BuildAgent = $false
 )
 
@@ -15,46 +15,43 @@ $DistFolderPath = "$CurrentDir\packages-dist"
 $PluginFilesPath = "$DistFolderPath\$DistroName"
 $BinFilesPath = "$PluginFilesPath\dist"
 
-$VersionSuffixCommand = ""
-if(-Not ([string]::IsNullOrEmpty($VersionSuffix))) {
-  $VersionSuffixCommand = "--version-suffix"
-}
-
 if (Test-Path $PluginFilesPath) {
-  Write-Information "Cleaning '$ProjectName' packages-dist folder '$PluginFilesPath'..." -InformationAction Continue
+  Write-Output "Cleaning '$ProjectName' packages-dist folder '$PluginFilesPath'..."
   Remove-Item $PluginFilesPath -Force -Recurse
 }
 
-Write-Information "`nPublishing '$ProjectName' component to '$BinFilesPath' ...`n" -InformationAction Continue
-dotnet publish "$ProjectName" --output "$BinFilesPath" --configuration $Configuration -p:Platform=$Platform $VersionSuffixCommand $VersionSuffix
+Write-Output "`nPublishing '$ProjectName' component to '$BinFilesPath' ...`n"
+dotnet publish "$ProjectName" --output "$BinFilesPath" --configuration $Configuration -p:Platform=$Platform
 
-Write-Information "`nPublishing '$ProjectName-Generator' component...`n" -InformationAction Continue
+Write-Output "`nPublishing '$ProjectName-Generator' component...`n"
 dotnet publish "$ProjectName-Generator" --output "$BinFilesPath" --configuration $Configuration -p:Platform=$Platform
 
 # Run Documentation
-Write-Information "`nGenerating entry.tp JSON and Documentation..." -InformationAction Continue
+Write-Output "`nGenerating entry.tp JSON and Documentation...`n"
 #dotnet run -p "$ProjectName-Generator" -o "$PluginFilesPath"
 & "$BinFilesPath\$ProjectName-Generator.exe" -o "$PluginFilesPath"
 
 # Copy Readme, CHANGELOG, image(s) to publish folder
 copy "README.md" "$PluginFilesPath"
 copy "CHANGELOG.md" "$PluginFilesPath"
-copy "airplane_takeoff24.png" "$PluginFilesPath"
+copy "LICENSE" "$PluginFilesPath"
+copy "$ProjectName\airplane_takeoff24.png" "$PluginFilesPath"
 
 # Get version
 $FileVersion = (Get-Command $BinFilesPath\$ProjectName.dll).FileVersionInfo.ProductVersion
 
 # Create TPP File
-$TppFile = "$DistFolderPath\$DistroName-$FileVersion.tpp"
+$TppFile = "$DistFolderPath\$DistroName-$FileVersion$VersionSuffix.tpp"
 if (Test-Path $TppFile) {
   Remove-Item $TppFile -Force
 }
+Write-Output "`nGenerating archive '$TppFile'..."
 & "C:\Program Files\7-Zip\7z.exe" a "$TppFile" "$DistFolderPath\*" -tzip `-xr!*.tpp
 
-if ($Clean) {
-  Write-Information "`nCleaning '$ProjectName-Generator' component....`n" -InformationAction Continue
+if (-not $NoClean) {
+  Write-Output "`nCleaning '$ProjectName-Generator' component....`n"
   dotnet clean "$ProjectName-Generator" --configuration $Configuration -p:Platform=$Platform -r "win-$Platform" /p:ValidateExecutableReferencesMatchSelfContained=false
-  Write-Information "`nCleaning '$ProjectName' component....`n" -InformationAction Continue
+  Write-Output "`nCleaning '$ProjectName' component....`n"
   dotnet clean "$ProjectName" --configuration $Configuration -p:Platform=$Platform -r "win-$Platform"
 }
 
